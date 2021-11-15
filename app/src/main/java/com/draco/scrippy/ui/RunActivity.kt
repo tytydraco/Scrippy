ad899ffa40254eef41b112f5a0e041b188225686
Tyler Nijmeh <tylernij@gmail.com>
2021-11-15 13:45:23 -0800

    Run activity skeleton
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ app/src/main/java/com/draco/scrippy/ui/RunActivity.kt
@@ -0,0 +1,12 @@
+package com.draco.scrippy.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.draco.scrippy.R
+
+class RunActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_run)
+    }
+}
